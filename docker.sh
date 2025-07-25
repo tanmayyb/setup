0cae466f8d51bd6e273a25133885d94bdb49b454
Tanmay Bishnoi <72982560+tanmayyb@users.noreply.github.com>
2025-07-25 16:37:56 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,4 +12,5 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+echo 'xhost +local:docker > /dev/null 2>&1' >> ~/.bashrc
 docker --version
