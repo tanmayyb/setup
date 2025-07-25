1e84653e1135359be81816b1644ab080236ca2bb
Tanmay Bishnoi <72982560+tanmayyb@users.noreply.github.com>
2025-07-25 16:34:59 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ set -e
 # Install dependencies
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
 sudo systemctl start docker
